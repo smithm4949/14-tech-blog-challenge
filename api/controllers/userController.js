c9307802c4c5648772f4ab22027e52f691b0d5ac
smithm4949 <smithm4949@gmail.com>
2023-02-27 22:14:27 -0500

    adding login/logout routes

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -56,3 +56,34 @@ exports.delete = function (req, res) {
   })
   .catch(err => res.status(400).json(err));
 }
+
+exports.login = function (req, res) {
+  User.findOne({
+    where: {username: req.body.username}
+  })
+  .then((user) => {
+    let passwordMatch = user.checkPassword(req.body.password);
+
+    if (passwordMatch) {
+      req.session.save(() => {
+        req.session.userId = user.id;
+        req.session.loggedIn = true;
+  
+        res.status(200).json({user, message: 'Succesfully logged in'});
+      });
+    } else {
+      res.status(400).json({message: 'Invalid password or email'});
+    }
+  })
+  .catch(err => res.status(400).json(err));
+}
+
+exports.logout = function (req, res) {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+}
