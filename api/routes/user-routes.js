d53c313e92e6b3b852c98dabaecdf7a04994423b
smithm4949 <smithm4949@gmail.com>
2023-02-27 22:19:32 -0500

    added userController to userRoutes

---
+++ api/routes/user-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userController = require('../controllers/userController');
+
+router.get('/', userController.getAll);
+router.post('/', userController.create);
+router.get('/:id', userController.getOne);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
+router.post('/login', userController.login);
+router.post('/logout', userController.login);
