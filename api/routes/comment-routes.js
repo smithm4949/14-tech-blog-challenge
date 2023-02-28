9ac7e810d9ca48ee40fd5a2493f0348cd247bf4c
smithm4949 <smithm4949@gmail.com>
2023-02-27 22:38:33 -0500

    fixed a type in comment routes

--- api/routes/comment-routes.js
+++ api/routes/comment-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const commentController = require('../controllers/commentController');
 
 router.get('/', commentController.getAll);
-router.comment('/', commentController.create);
+router.post('/', commentController.create);
 router.get('/:id', commentController.getOne);
 router.put('/:id', commentController.update);
 router.delete('/:id', commentController.delete);
