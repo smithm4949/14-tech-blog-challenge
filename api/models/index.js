6092e4149087feef17b2233871e3ad4082769848
smithm4949 <smithm4949@gmail.com>
2023-02-27 22:55:49 -0500

    adding relations into model index file, updated forgot to save all changes

--- api/models/index.js
+++ api/models/index.js
@@ -1,42 +1,14 @@
-// Import all of the models
-const Owner = require('./Owner');
-const Dog = require('./Dog');
-const Event = require('./Event');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-Owner.hasMany(Dog, {
-    foreignKey: 'owner_id',
+User.hasMany(Post, {
+    foreignKey: 'userId',
     onDelete: 'CASCADE'
 });
+Post.belongsTo(User);
 
-Dog.belongsTo(Owner, {
-  foreignKey: 'owner_id',
-    onDelete: 'CASCADE'
-});
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
 
-// Each event was created by and belongs to only one user (owner)
-Event.belongsTo(Owner, {
-  as: 'host',
-  foreignKey: 'host_id'
-});
-
-// Owners can create many events
-Owner.hasMany(Event, {
-  foreignKey: 'host_id'
-});
-
-// Events belong to many owners as people attending the event
-Event.belongsToMany(Owner, {
-  through: 'event_attendees',
-  as: 'attendees',
-  foreignKey: 'event_id',
-  otherKey: 'attendee_id'
-});
-
-// Owners can attend many events, regardless of who created the event
-Owner.belongsToMany(Event, {
-  through: 'event_attendees',
-  foreignKey: 'event_id',
-  otherKey: 'attendee_id'
-});
-
-module.exports = { Owner, Dog, Event }
+module.exports = { User, Post, Comment }
